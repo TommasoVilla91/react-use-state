4cd821a07f1d7cc1175eb9dcb2c8647422111d13
TommasoVilla91 <tommasovilla@yahoo.it>
2024-12-16 15:49:20 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  
+  return (
+    <>
+      <header>
+        <h1>Learn Web development</h1>
+      </header>
+      <main>
+        <section className="container">
+          <div className="row"></div>
+          <div className="card"></div>          
+        </section>
+      </main>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AppButtons.jsx
@@ -0,0 +1,9 @@
+function AppButtons() {
+
+
+    return (
+        <>
+
+        </>
+    )
+}
